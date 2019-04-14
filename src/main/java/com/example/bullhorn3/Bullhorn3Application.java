779ab0fa94f8960ba19cbca05f528fecbb2c2222
megok <rania.malak2018fd@gmail.com>
2019-04-13 21:49:25 -0400

    Initial commit

---
+++ src/main/java/com/example/bullhorn3/Bullhorn3Application.java
@@ -0,0 +1,13 @@
+package com.example.bullhorn3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bullhorn3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bullhorn3Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/bullhorn3/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.example.bullhorn3;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message,Long>{
+
+}
+
+
