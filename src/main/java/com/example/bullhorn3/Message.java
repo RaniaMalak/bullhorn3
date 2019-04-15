4713200b6eb30fe65409875c5b06bb1125a37bf3
megok <rania.malak2018fd@gmail.com>
2019-04-15 12:42:45 -0400

    content

--- src/main/java/com/example/bullhorn3/Message.java
+++ src/main/java/com/example/bullhorn3/Message.java
@@ -1,14 +1,18 @@
 package com.example.bullhorn3;
 
 
+
+
+//import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.text.DateFormat;
-import java.time.DateTimeException;
+//import java.text.DateFormat;
+//import java.time.DateTimeException;
 import java.util.Date;
 
 
@@ -24,6 +28,7 @@ public class Message {
 
     //  @NotNull
     // @Size(min=5)
+   // @DateTimeFormat
     private Date postdate;
 
     @NotNull
